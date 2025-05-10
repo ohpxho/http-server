90fe06c6cd720322247d24b5a4cfe199d141f623
ohpxho <lemuel.k.costuna.so@gmail.com>
2025-05-10 23:40:25 +0800

    Remove files that should be ignored from Git tracking

--- target/debug/deps/http_server-b66f89bee557b797.d
+++
@@ -1,5 +0,0 @@
-/Users/ohpxho/Documents/Dev/rust/http-server/target/debug/deps/http_server-b66f89bee557b797: main.rs
-
-/Users/ohpxho/Documents/Dev/rust/http-server/target/debug/deps/http_server-b66f89bee557b797.d: main.rs
-
-main.rs:
--- target/debug/http-server.d
+++
@@ -1,1 +0,0 @@
-/Users/ohpxho/Documents/Dev/rust/http-server/target/debug/http-server: /Users/ohpxho/Documents/Dev/rust/http-server/main.rs
