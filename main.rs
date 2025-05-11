db9a49b2ced05f0fd8ed99bcd2887d08a623d140
ohpxho <lemuel.k.costuna.so@gmail.com>
2025-05-11 22:16:32 +0800

    added logic for accessing specific URI

--- main.rs
+++ main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
@@ -11,13 +12,26 @@ fn main() {
     }
 }
 
-fn handle_connection(stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let lines: Vec<_> = buf_reader.lines().collect();
+    println!("Request received:");
+    for line in &lines {
+        println!("{}", line.as_ref().unwrap());
+    }
+    
+    let request_line = lines.first().unwrap().as_ref().unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
 
-    println!("Request: {http_request:#?}");
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
